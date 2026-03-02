b3bd7fe74b492bbb14d1ee06ad2e6a7666f6a84f
Xiaogang <maple.hl@gmail.com>
2026-03-02 21:08:46 +0800

    feat: Add `evdev` and `xev` library imports and their corresponding unit tests, along with build system configuration.

--- build.zig
+++ build.zig
@@ -28,4 +28,19 @@ pub fn build(b: *std.Build) !void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    const exe_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+    exe_tests.root_module.addImport("evdev", evdev_dep.module("evdev"));
+    exe_tests.root_module.addImport("xev",   xev_dep.module("xev"));
+
+    const run_exe_tests = b.addRunArtifact(exe_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_exe_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,17 @@
 const std = @import("std");
+const evdev = @import("evdev");
+const xev = @import("xev");
 
 pub fn main() !void {
     std.debug.print("Hello, kvm!\n", .{});
+}
+
+test "libxev test" {
+    var loop = try xev.Loop.init(.{});
+    defer loop.deinit();
+    try std.testing.expect(true);
+}
+
+test "evdev test" {
+    _ = evdev.Event;
 }
